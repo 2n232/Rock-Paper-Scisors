a2b944b21acb1d957c4c53aa312cbe67c01bfc52
2n232 <62015620+2n232@users.noreply.github.com>
2024-03-09 12:06:35 -0300

    Final

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let score = {computer: 0, player: 0};
+let score = { computer: 0, player: 0 };
 
 function getComputerChoice() {
-    let choice = Math.ceil(Math.random()*3);
+    let choice = Math.ceil(Math.random() * 3);
 
-    switch(choice) {
+    switch (choice) {
         case 1:
             return 'rock';
             break;
@@ -25,32 +25,38 @@ function playRound() {
     let computerSelection = getComputerChoice();
     let playerSelection = getPlayerSelection();
 
-    switch(playerSelection) {
+    switch (playerSelection) {
         case 'rock':
-            if(computerSelection==='scissors') {
-                score.player +=1;
-                return 'You Win! rock beats scissors'};
-            if(computerSelection=== 'paper') {
-                score.computer +=1;
-                return 'You Lose! paper beats rock'};
+            if (computerSelection === 'scissors') {
+                score.player += 1;
+                return 'You Win! rock beats scissors'
+            };
+            if (computerSelection === 'paper') {
+                score.computer += 1;
+                return 'You Lose! paper beats rock'
+            };
             return "It's a tie";
             break;
         case 'paper':
-            if(computerSelection=== 'rock') {
-                score.player +=1;
-                return 'You Win! paper beats rock'};
-            if(computerSelection==='scissors') {
-                score.computer +=1;
-                return 'You Lose! scissors beats paper'};
+            if (computerSelection === 'rock') {
+                score.player += 1;
+                return 'You Win! paper beats rock'
+            };
+            if (computerSelection === 'scissors') {
+                score.computer += 1;
+                return 'You Lose! scissors beats paper'
+            };
             return "It's a tie";
             break;
         case 'scissors':
-            if(computerSelection==='paper') {
-                score.player +=1;
-                return 'You Win! scissors beats paper'};
-            if(computerSelection=== 'rock') {
-                score.computer +=1;
-                return 'You Lose! rock beats scissors'};
+            if (computerSelection === 'paper') {
+                score.player += 1;
+                return 'You Win! scissors beats paper'
+            };
+            if (computerSelection === 'rock') {
+                score.computer += 1;
+                return 'You Lose! rock beats scissors'
+            };
             return "It's a tie";
             break;
     }
@@ -59,15 +65,15 @@ function playRound() {
 
 
 function playGame() {
-    
-    for(let i=1; i<=5; i++) {
+
+    for (let i = 1; i <= 5; i++) {
         playRound();
         console.log(`Points -- player: ${score.player}; computer: ${score.computer}`);
     }
 
-    if(score.computer > score.player) {
+    if (score.computer > score.player) {
         console.log(`You Lose! Computer: ${score.computer} -- You: ${score.player}`)
-    } else if (score.computer<score.player){
+    } else if (score.computer < score.player) {
         console.log(`You Win!  You: ${score.player} -- Computer: ${score.computer}`)
     } else {
         console.log(`It's a tie! You: ${score.player} -- Computer: ${score.computer}`)
